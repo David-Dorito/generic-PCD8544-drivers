a516a05aeeeee9426d80912f6456944d88ef46a1
David-Dorito <fercone75@gmail.com>
2026-09-16 22:35:49 +0200

    fix: fixed function names in source file

--- pcd8544_driver.c
+++ pcd8544_driver.c
@@ -1,22 +1,22 @@
 #include <stddef.h>
 #include "pcd8544_driver.h"
 
-#define LOW	 0
+#define LOW 0
 #define HIGH 1
 
-#define SET_XADDR_0		0b10000000
-#define SET_YADDR_0		0b01000000
-#define SET_POWERDOWN	0b00100100
-#define SET_POWERUP		0b00100000
-#define SET_EXTENDINST	0b00100001
-#define SET_BASICINST	0b00100000
-#define SET_VOP			0b10000000
-#define SET_TC			0b00000100
+#define SET_XADDR_0 0b10000000
+#define SET_YADDR_0 0b01000000
+#define SET_POWERDOWN 0b00100100
+#define SET_POWERUP 0b00100000
+#define SET_EXTENDINST 0b00100001
+#define SET_BASICINST 0b00100000
+#define SET_VOP 0b10000000
+#define SET_TC 0b00000100
 #define SET_DISPLAYMODE 0b00001000
-#define SET_BIAS_1_48	0b00010011
+#define SET_BIAS_1_48 0b00010011
 
 /*************************************\
-  fn: @PCD8544_Init
+  fn: @Pcd8544_Init
 
   param1 Pcd8544_Handle*: the handle of the display
 
@@ -25,10 +25,10 @@
   desc: turns the display on using the sequence described in the datasheet and sets some basic
 configurations
 
-  note: sets the contrast to PCD8544_CONTRAST_DEFAULT found in the header file
+  note: sets the contrast to Pcd8544_CONTRAST_DEFAULT found in the header file
 
 \**************************************/
-Pcd8544_Status PCD8544_Init(Pcd8544_Handle* self) {
+Pcd8544_Status Pcd8544_Init(Pcd8544_Handle* self) {
 	Pcd8544_Status status = PCD8544_OK;
 
 	// Control VCC if user provides the pin, otherwise assume external 3.3V
@@ -107,7 +107,7 @@ Pcd8544_Status PCD8544_Init(Pcd8544_Handle* self) {
 }
 
 /*************************************\
-  fn: @PCD8544_Deinit
+  fn: @Pcd8544_Deinit
 
   param1 Pcd8544_Handle*: the handle of the display
 
@@ -118,7 +118,7 @@ Pcd8544_Status PCD8544_Init(Pcd8544_Handle* self) {
   note: also pulls chip select to HIGH and Data/Command to LOW
 
 \**************************************/
-void PCD8544_Deinit(Pcd8544_Handle* self) {
+void Pcd8544_Deinit(Pcd8544_Handle* self) {
 	self->Drivers->GpioWritePin(self->CsPin, HIGH);
 	self->Drivers->GpioWritePin(self->DcPin, LOW);
 	if (self->LedPin != NULL)
@@ -128,7 +128,7 @@ void PCD8544_Deinit(Pcd8544_Handle* self) {
 }
 
 /*************************************\
-  fn: @PCD8544_SetBacklight
+  fn: @Pcd8544_SetBacklight
 
   param1 Pcd8544_Handle*: the handle of the display
   param2 u8: enable or disable sleep mode of the display
@@ -140,12 +140,12 @@ void PCD8544_Deinit(Pcd8544_Handle* self) {
   note: function is very basic, just a wrapper for GpioWritePin() with the LED pin as the arg
 
 \**************************************/
-void PCD8544_SetBacklight(Pcd8544_Handle* self, uint8_t isEnabled) {
+void Pcd8544_SetBacklight(Pcd8544_Handle* self, uint8_t isEnabled) {
 	self->Drivers->GpioWritePin(self->LedPin, !isEnabled);
 }
 
 /*************************************\
-  fn: @PCD8544_SetSleepMode
+  fn: @Pcd8544_SetSleepMode
 
   param1 Pcd8544_Handle*: the handle of the display
   param2 u8: enable or disable sleep mode of the display
@@ -155,10 +155,10 @@ void PCD8544_SetBacklight(Pcd8544_Handle* self, uint8_t isEnabled) {
   desc: enables or disables the sleep mode of the display
 
   note: display will stop displaying an image if turned on but RAM will NOT get cleared,
-		so once you turn off sleep mode it will start displaying the image again
+        so once you turn off sleep mode it will start displaying the image again
 
 \**************************************/
-Pcd8544_Status PCD8544_SetSleepMode(Pcd8544_Handle* self, uint8_t isEnabled) {
+Pcd8544_Status Pcd8544_SetSleepMode(Pcd8544_Handle* self, uint8_t isEnabled) {
 	Pcd8544_Status status = PCD8544_OK;
 
 	self->Drivers->GpioWritePin(self->CsPin, LOW);
@@ -181,7 +181,7 @@ Pcd8544_Status PCD8544_SetSleepMode(Pcd8544_Handle* self, uint8_t isEnabled) {
 }
 
 /*************************************\
-  fn: @PCD8544_SetDisplayMode
+  fn: @Pcd8544_SetDisplayMode
 
   param1 Pcd8544_Handle*: the handle of the display
   param2 u8: new mode of the display
@@ -194,7 +194,7 @@ Pcd8544_Status PCD8544_SetSleepMode(Pcd8544_Handle* self, uint8_t isEnabled) {
 what they do
 
 \**************************************/
-Pcd8544_Status PCD8544_SetDisplayMode(Pcd8544_Handle* self, uint8_t mode) {
+Pcd8544_Status Pcd8544_SetDisplayMode(Pcd8544_Handle* self, uint8_t mode) {
 	Pcd8544_Status status = PCD8544_OK;
 
 	self->Drivers->GpioWritePin(self->CsPin, LOW);
@@ -213,7 +213,7 @@ Pcd8544_Status PCD8544_SetDisplayMode(Pcd8544_Handle* self, uint8_t mode) {
 }
 
 /*************************************\
-  fn: @PCD8544_SetTempCoeff
+  fn: @Pcd8544_SetTempCoeff
 
   param1 Pcd8544_Handle*: the handle of the display
   param2 u8: the temperature coefficient
@@ -222,11 +222,11 @@ Pcd8544_Status PCD8544_SetDisplayMode(Pcd8544_Handle* self, uint8_t mode) {
 
   desc: sets the temperature coefficient of the display
 
-  note: use the PCD8544_TEMPCOEFF_XXX macros as the coefficient arg, view the datasheet for a description
+  note: use the Pcd8544_TEMPCOEFF_XXX macros as the coefficient arg, view the datasheet for a description
 of what they do
 
 \**************************************/
-Pcd8544_Status PCD8544_SetTempCoeff(Pcd8544_Handle* self, uint8_t coefficient) {
+Pcd8544_Status Pcd8544_SetTempCoeff(Pcd8544_Handle* self, uint8_t coefficient) {
 	Pcd8544_Status status = PCD8544_OK;
 
 	self->Drivers->GpioWritePin(self->CsPin, LOW);
@@ -245,7 +245,7 @@ Pcd8544_Status PCD8544_SetTempCoeff(Pcd8544_Handle* self, uint8_t coefficient) {
 }
 
 /*************************************\
-  fn: @PCD8544_SetContrast
+  fn: @Pcd8544_SetContrast
 
   param1 Pcd8544_Handle*: the handle of the display
   param2 u8: new contrast of the display
@@ -257,7 +257,7 @@ Pcd8544_Status PCD8544_SetTempCoeff(Pcd8544_Handle* self, uint8_t coefficient) {
   note:
 
 \**************************************/
-Pcd8544_Status PCD8544_SetContrast(Pcd8544_Handle* self, uint8_t contrast) {
+Pcd8544_Status Pcd8544_SetContrast(Pcd8544_Handle* self, uint8_t contrast) {
 	Pcd8544_Status status = PCD8544_OK;
 
 	self->Drivers->GpioWritePin(self->CsPin, LOW);
@@ -291,7 +291,7 @@ Pcd8544_Status PCD8544_SetContrast(Pcd8544_Handle* self, uint8_t contrast) {
 }
 
 /*************************************\
-  fn: @PCD8544_TogglePixelColor
+  fn: @Pcd8544_TogglePixelColor
 
   param1 Pcd8544_Handle*: the handle of the display
   param2 u8: x position of the pixel
@@ -304,7 +304,7 @@ Pcd8544_Status PCD8544_SetContrast(Pcd8544_Handle* self, uint8_t contrast) {
   note: early returns nothing to avoid memory corruption if posX >= width or posY >= height
 
 \**************************************/
-void PCD8544_TogglePixelColor(Pcd8544_Handle* self, uint8_t posX, uint8_t posY) {
+void Pcd8544_TogglePixelColor(Pcd8544_Handle* self, uint8_t posX, uint8_t posY) {
 	if (posX >= PCD8544_SCREEN_WIDTH || posY >= PCD8544_SCREEN_HEIGHT)
 		return;
 
@@ -314,7 +314,7 @@ void PCD8544_TogglePixelColor(Pcd8544_Handle* self, uint8_t posX, uint8_t posY) 
 }
 
 /*************************************\
-  fn: @PCD8544_SetPixelColor
+  fn: @Pcd8544_SetPixelColor
 
   param1 Pcd8544_Handle*: the handle of the display
   param2 u8: the colour of the pixel
@@ -328,7 +328,7 @@ void PCD8544_TogglePixelColor(Pcd8544_Handle* self, uint8_t posX, uint8_t posY) 
   note: early returns nothing to avoid memory corruption if posX >= width or posY >= height
 
 \**************************************/
-void PCD8544_SetPixelColor(Pcd8544_Handle* self, uint8_t posX, uint8_t posY, uint8_t isBlack) {
+void Pcd8544_SetPixelColor(Pcd8544_Handle* self, uint8_t posX, uint8_t posY, uint8_t isBlack) {
 	if (posX >= PCD8544_SCREEN_WIDTH || posY >= PCD8544_SCREEN_HEIGHT)
 		return;
 
@@ -341,7 +341,7 @@ void PCD8544_SetPixelColor(Pcd8544_Handle* self, uint8_t posX, uint8_t posY, uin
 }
 
 /*************************************\
-  fn: @PCD8544_GetPixelColor
+  fn: @Pcd8544_GetPixelColor
 
   param1 Pcd8544_Handle*: the handle of the display
   param2 u8: x position of the pixel
@@ -354,7 +354,7 @@ void PCD8544_SetPixelColor(Pcd8544_Handle* self, uint8_t posX, uint8_t posY, uin
   note: returns 0 if posX >= width or posY >= height to avoid memory corruption
 
 \**************************************/
-uint8_t PCD8544_GetPixelColor(Pcd8544_Handle* self, uint8_t posX, uint8_t posY) {
+uint8_t Pcd8544_GetPixelColor(Pcd8544_Handle* self, uint8_t posX, uint8_t posY) {
 	if (posX >= PCD8544_SCREEN_WIDTH || posY >= PCD8544_SCREEN_HEIGHT)
 		return 0;
 
@@ -364,7 +364,7 @@ uint8_t PCD8544_GetPixelColor(Pcd8544_Handle* self, uint8_t posX, uint8_t posY) 
 }
 
 /*************************************\
-  fn: @PCD8544_FillScreenColor
+  fn: @Pcd8544_FillScreenColor
 
   param1 Pcd8544_Handle*: the handle of the display
   param2 u8: the colour to set every pixel to
@@ -376,14 +376,14 @@ uint8_t PCD8544_GetPixelColor(Pcd8544_Handle* self, uint8_t posX, uint8_t posY) 
   note:
 
 \**************************************/
-void PCD8544_FillScreenColor(Pcd8544_Handle* self, uint8_t isBlack) {
+void Pcd8544_FillScreenColor(Pcd8544_Handle* self, uint8_t isBlack) {
 	isBlack = isBlack ? 0xFF : 0x00;
 	for (uint16_t i = 0; i < PCD8544_SCREEN_SIZE; i++)
 		self->FrameBuffer[i] = isBlack;
 }
 
 /*************************************\
-  fn: @PCD8544_UpdateScreen
+  fn: @Pcd8544_UpdateScreen
 
   param1 Pcd8544_Handle*: the handle of the display
 
@@ -396,7 +396,7 @@ dont actually write to the display RAM, it just writes to the buffer in the hand
 whole buffer to the displays RAM
 
 \**************************************/
-Pcd8544_Status PCD8544_UpdateScreen(Pcd8544_Handle* self) {
+Pcd8544_Status Pcd8544_UpdateScreen(Pcd8544_Handle* self) {
 	Pcd8544_Status status = PCD8544_OK;
 
 	self->Drivers->GpioWritePin(self->CsPin, LOW);
@@ -418,7 +418,7 @@ Pcd8544_Status PCD8544_UpdateScreen(Pcd8544_Handle* self) {
 
 	self->Drivers->GpioWritePin(self->DcPin, HIGH);
 	status = self->Drivers->SpiTransmit(self->Spi, self->FrameBuffer, PCD8544_SCREEN_SIZE,
-										self->SpiTransmitTimeout);
+	                                    self->SpiTransmitTimeout);
 	self->Drivers->GpioWritePin(self->DcPin, LOW);
 	if (status != PCD8544_OK) {
 		self->Drivers->GpioWritePin(self->CsPin, HIGH);
